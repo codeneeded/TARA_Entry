07e1f2a643f0ecfd18935c5e0099ac63f97ab88f
Akshay <akshay.iyer2018@gmail.com>
2024-10-21 11:35:34 -0400

    Stop tracking saved_R_data and add it to .gitignore

--- Scripts/Grant_NK.R
+++ Scripts/Grant_NK.R
@@ -3,6 +3,7 @@
 library(Nebulosa)
 library(Seurat)
 library(SingleCellExperiment)
+library(viridis)
 library(scater)
 library(tidyverse)
 library(Matrix)
@@ -24,56 +25,169 @@ library(reticulate)
 library(circlize)
 library(ComplexHeatmap)
 library(readxl)
+library(Polychrome)
+
 ##set path to load data
 
 
-setwd('C:/Users/ammas/Documents/TARA_Entry/WNN')
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN')
 
-load.path <- "C:/Users/ammas/Documents/TARA_Entry/saved_R_data/"
+load.path <- "C:/Users/axi313/Documents/TARA_Entry/saved_R_data/"
 
 load(paste0(load.path,'Seuratv5_WNN_Complete.RData'))
 
 ##### Subset NK Cells #######
 
-setwd('C:/Users/ammas/Documents/TARA_Entry/WNN/NK_Only')
-NK <- subset(seurat_isotype, idents=c(4,15,16,19))
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only')
+NK <- subset(seurat_isotype,idents=c(4,15,16,19),subset = Condition == 'HEI'| Condition == 'HEU')
 
-DimPlot(NK, reduction='wnn.umap',label=T,label.size = 4.5,repel=T, cols='polychrome', split.by = 'Condition')
+
+DimPlot(NK, reduction='wnn.umap',label=T,label.size = 4.5,repel=T, cols=c('green','brown','darkgreen','yellow'), split.by = 'Condition')
 ggsave('NK_WNN_ClusterPlot_res0.8.png',dpi=500, width = 10)
+DimPlot(NK, reduction='wnn.umap',label=T,label.size = 4.5,repel=T, cols=c('green','brown','darkgreen','yellow'))
+ggsave('NK_WNN_ClusterPlot_res0.8_all.png',dpi=500)
 
 ### Feature Plots
 
 ### RNA
-setwd('C:/Users/ammas/Documents/TARA_Entry/WNN/NK_Only/Feature_Plot/RNA')
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/Feature_Plot/RNA')
 
 DefaultAssay(NK) <- 'RNA'
 
 rna.features <-  c("NCAM1", "FCGR3A", "B3GAT1", "KLRC1", "KLRC2", "KLRK1", "NCR1", 
-                   "NCR2", "NCR3", "PRF1", "GZMB", "HLA-DRB5", "HLA-DRB1", "FCER1G", "LAG3"
+                   "NCR2", "NCR3", "PRF1", "GZMB", "HLA-DRB5", "HLA-DRB1", "FCER1G", "LAG3",
+                   "FASLG","KLRG1","KLRB1","SIGLEC7","GNLY"
 )
+
+
 # Feature Plots
 
 
 for (i in rna.features) {
-  fea.pl <- FeaturePlot(NK, reduction = 'wnn.umap', features = i)
-  ggsave(paste0(i,'_Featureplot.png'),dpi=500, width = 8, fea.pl)
+  pal <- viridis(n = 10, option = "A")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Magma.png'),dpi=500, width = 10, fea.pl)
+  pal <- viridis(n = 10, option = "G")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Mako.png'),dpi=500, width = 10, fea.pl)
+  pal <- viridis(n = 10, option = "D")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Viridis.png'),dpi=500, width = 10, fea.pl)
 }
-### ADT
-setwd('C:/Users/ammas/Documents/TARA_Entry/WNN/NK_Only/Feature_Plot/ADT')
 
-DefaultAssay(NK) <- 'ADT'
+# VLN Plots
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/VLN_Plot/RNA')
 
-adt.features <-  c( "LAG3",
-                   "NCAM1", "FCGR3A", "B3GAT1","KLRK1", "NCR1"
+for (i in rna.features) {
+
+  vln.pl <-VlnPlot_scCustom(NK,features = i ,split.by = 'Condition')
+  ggsave(paste0(i,'_VLNplot_HEIvsHEU.png'),dpi=500, width = 10, vln.pl)
+}
+
+#### Group Feature PLOTS ####
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/Group_Plots/RNA')
+
+### RNA 
+cols <- c("lavender", "lightpink", "lightcoral", "lightgoldenrodyellow", "lightgreen")
+fea_all <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = rna.features, colors_use = cols,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_RNA_Light.png',dpi=500, width = 13,height = 12, fea_all)
+pal <- viridis(n = 10, option = "D")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = rna.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_RNA_viridis.png',dpi=500, width = 13,height = 12, fea_al_custom)
+pal <- viridis(n = 10, option = "A")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = rna.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_RNA_magma.png',dpi=500, width = 13,height = 12, fea_al_custom)
+pal <- viridis(n = 10, option = "G")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = rna.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_RNA_mako.png',dpi=500, width = 13,height = 12, fea_al_custom)
+vln_plot_stk <-Stacked_VlnPlot(seurat_object = NK, features = rna.features, x_lab_rotate = TRUE,pt.size = 0.2)
+ggsave('NK_Stacked_VLN_ALL_RNA.png',dpi=500, width = 13,height = 23, vln_plot_stk)
+
+
+rdge <- RidgePlot(NK, features = rna.features,assay='RNA',log=TRUE,cols=c('green','brown','darkgreen','yellow'))
+ggsave(filename='NK_rdge_plot_RNA.png',dpi=500, width =9 ,height = 13,rdge)
+
+clust_dot_plot <- Clustered_DotPlot(seurat_object = NK, features = rna.features, k=4,
+                                    colors_use_idents = c('green','brown','darkgreen','yellow'))
+png("NK_Clust_dotplot_RNA.png",width=8,height=9,units="in",res=1200)
+draw(clust_dot_plot[[2]])
+dev.off()
+
+clust_dot_plot_2 <- Clustered_DotPlot(seurat_object = NK, features = rna.features, split.by = 'Viral_Load_Category',  cluster_ident = FALSE,
+                          colors_use_idents='polychrome'
 )
+png("NK_Clust_dotplot_RNA_VL.png",width=8,height=7,units="in",res=1200)
+draw(clust_dot_plot_2[[2]])
+dev.off()
+
+library(grid)
+grid.ls()
+c_dp[[2]]$matrix_25$
+#### ADT #### 
+DefaultAssay(NK) <- 'ADT'
+adt.features <-  c( "LAG3",
+                    "NCAM1", "FCGR3A", "B3GAT1","KLRK1", "NCR1"
+)
+
 # Feature Plots
 
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/Feature_Plot/ADT')
 
 for (i in adt.features) {
-  fea.pl <- FeaturePlot(NK, reduction = 'wnn.umap', features = i)
-  ggsave(paste0(i,'_Featureplot.png'),dpi=500, width = 8, fea.pl)
+  pal <- viridis(n = 10, option = "A")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Magma.png'),dpi=500, width = 10, fea.pl)
+  pal <- viridis(n = 10, option = "G")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Mako.png'),dpi=500, width = 10, fea.pl)
+  pal <- viridis(n = 10, option = "D")
+  fea.pl <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = i,split.by = 'Condition', 
+                                 colors_use = pal,order=TRUE)
+  ggsave(paste0(i,'_Featureplot_HEIvsHEU_Viridis.png'),dpi=500, width = 10, fea.pl)
 }
 
+# VLN Plots
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/VLN_Plot/ADT')
+
+for (i in adt.features) {
+  
+  vln.pl <-VlnPlot_scCustom(NK,features = i ,split.by = 'Condition')
+  ggsave(paste0(i,'_VLNplot_HEIvsHEU.png'),dpi=500, width = 10, vln.pl)
+}
+
+#### Group Feature PLOTS ####
+setwd('C:/Users/axi313/Documents/TARA_Entry/WNN/NK_Only/Group_Plots/ADT')
+
+
+
+cols <- c("lavender", "lightpink", "lightcoral", "lightgoldenrodyellow", "lightgreen")
+fea_all <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = adt.features, colors_use = cols,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_ADT_Light.png',dpi=500, width = 13,height = 12, fea_all)
+pal <- viridis(n = 10, option = "D")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = adt.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_ADT_viridis.png',dpi=500, width = 13,height = 12, fea_al_custom)
+pal <- viridis(n = 10, option = "A")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = adt.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_ADT_magma.png',dpi=500, width = 13,height = 12, fea_al_custom)
+pal <- viridis(n = 10, option = "G")
+fea_al_custom <- FeaturePlot_scCustom(NK, reduction = 'wnn.umap', features = adt.features, colors_use = pal,keep.scale = 'feature',order=TRUE)
+ggsave('NK_Featureplot_ALL_ADT_mako.png',dpi=500, width = 13,height = 12, fea_al_custom)
+vln_plot_stk <-Stacked_VlnPlot(seurat_object = NK, features = adt.features, x_lab_rotate = TRUE,pt.size = 0.2)
+ggsave('NK_Stacked_VLN_ALL_ADT.png',dpi=500, width = 13,height = 23, vln_plot_stk)
+
+
+rdge <- RidgePlot(NK, features = adt.features,assay='ADT',log=TRUE,cols =  c('green','brown','darkgreen','yellow'))
+ggsave(filename='NK_rdge_plot_ADT.png',dpi=500, width =10 ,height = 8,rdge)
+
+clust_dot_plot <- Clustered_DotPlot(seurat_object = NK, features = adt.features,colors_use_idents =  c('green','brown','darkgreen','yellow'))
+png("NK_Clust_dotplot_ADT.png",width=8,height=9,units="in",res=1200)
+draw(clust_dot_plot[[2]])
+dev.off()
 
 
 ############## NK PLOTS #########################
